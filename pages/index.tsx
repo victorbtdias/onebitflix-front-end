892ab598f337719e8bad8d10fe004094da4c0a40
victorbtdias <victorbtdias159@hotmail.com>
2023-01-18 13:48:31 -0300

    Adicionando header da página homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import HeaderNoAuth from "../src/components/homeNoAuth/headerNoAuth";
 import Head from "next/head";
 import styles from "../styles/HomeNoAuth.module.scss";
 
@@ -5,9 +6,17 @@ const HomeNoAuth = () => {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+        <meta
+          name="description"
+          content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."
+        ></meta>
       </Head>
-      <main></main>
+      <main>
+        <HeaderNoAuth />
+      </main>
     </>
   );
 };
