e0e4bd71add51e4079e2429ece15d10703c60e00
victorbtdias <victorbtdias159@hotmail.com>
2023-01-27 11:39:16 -0300

    Criação dos elementos da seção de favoritos, do seu estilo visual e também dos métodos de favorito - Seção favoritos

---
+++ src/components/homeAuth/favoriteCategory/index.tsx
@@ -0,0 +1,30 @@
+import courseService from "@/src/services/courseService";
+import useSWR from "swr";
+import styles from "../../../../styles/slideCategory.module.scss";
+import SlideComponent from "../../common/slideComponent";
+
+const FavoritesCategory = function () {
+  const { data, error } = useSWR("/favorites", courseService.getFavCourses);
+
+  if (error) return error;
+  if (!data)
+    return (
+      <>
+        <p>Loading...</p>
+      </>
+    );
+  return (
+    <>
+      <p className={styles.titleCategory}>Minha Lista</p>
+      {data.data.courses.length >= 1 ? (
+        <SlideComponent course={data.data.courses} />
+      ) : (
+        <p className="h5 text-center pt-3">
+          <strong>Você não tem nenhum curso na lista</strong>
+        </p>
+      )}
+    </>
+  );
+};
+
+export default FavoritesCategory;
