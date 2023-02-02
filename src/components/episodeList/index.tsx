a0a2c93e17eb5ea6a499c6ff7dacef710e4c4db0
victorbtdias <victorbtdias159@hotmail.com>
2023-02-02 11:43:41 -0300

    Criação dos elementos da página de espisódio, do seu estilo visual e também da sua responsividade completa - Episódio

--- src/components/episodeList/index.tsx
+++ src/components/episodeList/index.tsx
@@ -1,11 +1,15 @@
 import styles from "./styles.module.scss";
-import { EpisodeType } from "../../services/courseService";
+import { CourseType, EpisodeType } from "../../services/courseService";
+import { useRouter } from "next/router";
 
 interface props {
   episode: EpisodeType;
+  course: CourseType;
 }
 
-const EpisodeList = function ({ episode }: props) {
+const EpisodeList = function ({ episode, course }: props) {
+  const router = useRouter();
+
   const handleSecondsToMin = (totalSeconds: number) => {
     const minutes = Math.floor(totalSeconds / 60);
 
@@ -19,9 +23,13 @@ const EpisodeList = function ({ episode }: props) {
 
     return result;
   };
+
+  const handleEpisodePlayer = () => {
+    router.push(`/courses/episode/${episode.order - 1}?courseid=${course.id}`);
+  };
   return (
     <>
-      <div className={styles.episodeCard}>
+      <div className={styles.episodeCard} onClick={handleEpisodePlayer}>
         <div className={styles.episodeOrderTime}>
           <p className={styles.episodeOrder}>Episódio Nº {episode.order}</p>
           <p className={styles.episodeTime}>
